c39891522b862eb91b0837480b3ecbc73a989373
Alex <Dialexio@users.noreply.github.com>
2017-10-02 19:42:04 -0400

    Updated with code from newer Example skin
    This allows Retina-quality logos to be used and modernizes the code a bit more.
    
    Other minor tweaks have also been made.

--- Jony/Jony.skin.php
+++ Jony/Jony.skin.php
@@ -1,12 +1,5 @@
 <?php
 /**
- * Skin file for the Jony skin.
- *
- * @file
- * @ingroup Skins
- */
-
-/**
  * SkinTemplate class for the Jony skin
  *
  * @ingroup Skins
@@ -15,215 +8,29 @@ class SkinJony extends SkinTemplate {
 	public $skinname = 'jony', $stylename = 'Jony',
 		$template = 'JonyTemplate', $useHeadElement = true;
 
-/*
-	public function initPage( OutputPage $out ) {
-		parent::initPage( $out );
-		$out->addModules( 'skins.jony.js' );
-	}
-*/
-
 	/**
 	 * Add CSS via ResourceLoader
 	 *
 	 * @param $out OutputPage
 	 */
+	public function initPage( OutputPage $out ) {
+
+		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0' );
+
+		$out->addModuleStyles( array(
+			'mediawiki.skinning.interface',
+			'mediawiki.skinning.content.externallinks',
+			'skins.jony'
+		) );
+/*		$out->addModules( array(
+			'skins.jony.js'
+		) );*/
+	}
+
+	/**
+	 * @param $out OutputPage
+	 */
 	function setupSkinUserCss( OutputPage $out ) {
 		parent::setupSkinUserCss( $out );
-		$out->addModuleStyles( array(
-			'mediawiki.skinning.interface', 'skins.jony'
-		) );
-	}
-}
-
-/**
- * BaseTemplate class for the Jony skin
- *
- * @ingroup Skins
- */
-class JonyTemplate extends BaseTemplate {
-	/**
-	 * Outputs a single sidebar portlet of any kind.
-	 */
-	private function outputPortlet( $box ) {
-		if ( !$box['content'] ) {
-			return;
-		}
-
-		?>
-		<div
-			role="navigation"
-			class="mw-portlet"
-			id="<?php echo Sanitizer::escapeId( $box['id'] ) ?>"
-			<?php echo Linker::tooltip( $box['id'] ) ?>
-		>
-			<h3>
-				<?php
-				if ( isset( $box['headerMessage'] ) ) {
-					$this->msg( $box['headerMessage'] );
-				} else {
-					echo htmlspecialchars( $box['header'] );
-				}
-				?>
-			</h3>
-
-			<?php
-			if ( is_array( $box['content'] ) ) {
-				echo '<ul>';
-				foreach ( $box['content'] as $key => $item ) {
-					echo $this->makeListItem( $key, $item );
-				}
-				echo '</ul>';
-			} else {
-				echo $box['content'];
-			}?>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Outputs the entire contents of the page
-	 */
-	public function execute() {
-		$this->html( 'headelement' ) ?>
-
-	<?php if ( $this->data['newtalk'] ) { ?>
-		<div id="usermessage" onclick="$(this).slideUp(100);">
-			<h4><?php $this->html( 'newtalk' ) ?></h4>
-			<p>Clicking this alert will hide it until the next page loads. You must view the message to permanently clear this.</p>
-		</div>
-	<?php } ?>
-
-		<div id="mw-wrapper">
-			<div id="p-logo" role="banner">
-				<a href="<?php echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] ) ?>"
-					<?php echo Xml::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'p-logo' ) ) ?>
-				>
-					<img
-						src="<?php $this->text( 'logopath' ) ?>"
-						alt="<?php $this->text( 'sitename' ) ?>"
-					/>
-				</a>
-			</div>
-
-
-			<div id="mw-panel">
-				<?php
-				foreach ( $this->getSidebar() as $boxName => $box ) {
-					$this->outputPortlet( $box );
-				}
-				?>
-			</div>
-
-
-			<div id="mw-toolbar">
-				<div id="mw-status" ondblclick="$('html, body').animate({ scrollTop: 0 }, 'fast');">
-					<?php
-
-					$this->outputPortlet( array(
-						'id' => 'p-personal',
-						'headerMessage' => 'personaltools',
-						'content' => $this->getPersonalTools(),
-					) );
-					?>
-				</div>
-
-				<div id="mw-tabs">
-					<?php
-
-					$this->outputPortlet( array(
-						'id' => 'p-namespaces',
-						'headerMessage' => 'namespaces',
-						'content' => $this->data['content_navigation']['namespaces'],
-					) );
-					$this->outputPortlet( array(
-						'id' => 'p-variants',
-						'headerMessage' => 'variants',
-						'content' => $this->data['content_navigation']['variants'],
-					) );
-					$this->outputPortlet( array(
-						'id' => 'p-views',
-						'headerMessage' => 'views',
-						'content' => $this->data['content_navigation']['views'],
-					) );
-					$this->outputPortlet( array(
-						'id' => 'p-actions',
-						'headerMessage' => 'actions',
-						'content' => $this->data['content_navigation']['actions'],
-					) );
-					?>
-					<form
-						action="<?php $this->text( 'wgScript' ) ?>"
-						class="mw-portlet"
-						id="p-search">
-							<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>" />
-
-							<h3><label for="searchInput"><?php $this->msg( 'search' ) ?></label></h3>
-
-							<?php echo $this->makeSearchInput( array( "id" => "searchInput" ) ) ?>
-							<?php echo $this->makeSearchButton( 'go' ) ?>
-					</form>
-				</div>
-			</div>
-
-			<div id="mw-toolbar-spacer">
-			</div>
-
-			<div class="mw-body" role="main">
-				<?php if ( $this->data['sitenotice'] ) { ?>
-					<div id="siteNotice"><?php $this->html( 'sitenotice' ) ?></div>
-				<?php } ?>
-
-				<h1 class="firstHeading">
-					<?php $this->html( 'title' ) ?>
-				</h1>
-
-				<div id="siteSub"><?php $this->msg( 'tagline' ) ?></div>
-
-				<div class="mw-body-content">
-					<div id="contentSub">
-						<?php if ( $this->data['subtitle'] ) { ?>
-							<p><?php $this->html( 'subtitle' ) ?></p>
-						<?php } ?>
-						<?php if ( $this->data['undelete'] ) { ?>
-							<p><?php $this->html( 'undelete' ) ?></p>
-						<?php } ?>
-					</div>
-
-					<?php $this->html( 'bodytext' ) ?>
-
-					<?php $this->html( 'catlinks' ) ?>
-
-					<?php $this->html( 'dataAfterContent' ); ?>
-
-				</div>
-			</div>
-
-			<div id="mw-footer">
-				<ul role="contentinfo">
-					<?php foreach ( $this->getFooterIcons( 'icononly' ) as $blockName => $footerIcons ) { ?>
-						<li>
-							<?php
-							foreach ( $footerIcons as $icon ) {
-								echo $this->getSkin()->makeFooterIcon( $icon );
-							}
-							?>
-						</li>
-					<?php } ?>
-				</ul>
-
-				<?php foreach ( $this->getFooterLinks() as $category => $links ) { ?>
-					<ul class="footer-privacy">
-						<?php foreach ( $links as $key ) { ?>
-							<li><?php $this->html( $key ) ?></li>
-						<?php } ?>
-					</ul>
-				<?php } ?>
-			</div>
-		</div>
-
-		<?php $this->printTrail() ?>
-		</body></html>
-
-		<?php
 	}
 }
--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # Jony
-A skin for MediaWiki, inspired by the user interface of newer versions of iOS.
+A skin for MediaWiki, inspired by the user interface of modern versions of iOS.
 
-This skin has only been tested on the MediaWiki 1.24, 1.25, and 1.26 branches. This skin may work with other versions, but compatibility is not ensured.
+This skin has only been tested on MediaWiki 1.28.x branch. This skin may work with other versions, but compatibility is not ensured.
 
 ## Installation
 To install this theme, copy the "Jony" directory into your MediaWiki installation's "skins" directory.
 
-For MediaWiki 1.24, the following line must be also be added to your LocalSettings.php:
-```php
-    require_once( "$IP/skins/Jony/Jony.php" );
-```
-
-For MediaWiki 1.25 and newer, it is recommended to use this line instead:
+The following line must be also be added to your LocalSettings.php:
 ```php
     wfLoadSkin( "Jony" );
 ```
@@ -19,4 +14,4 @@ For MediaWiki 1.25 and newer, it is recommended to use this line instead:
 ## Variables
 As most of the styling is written in [LESS](http://lesscss.org/), this skin makes use of variables that may be customized to your liking. These are primarily offered to allow for easy color customization, but may in future updates.
 
-Under the terms of the [GNU General Public License 2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC1), you are required to publish all modifications under the same license. I hereby waive this requirement, provided the only modifications made are to value of the LESS variables. If any other portion of this skin is modified, the terms of the GNU General Public License 2.0 still apply.
+Under the terms of the [GNU General Public License 2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC1), you are required to publish all modifications under the same license. I hereby waive this requirement, provided the only modifications made are to the value of the LESS variables. If any other portion of this skin is modified, the terms of the GNU General Public License 2.0 still apply.
